f42cd6717ef034cb614c62f6b66bafe4772b8d24
Kirill Nikolaev <kir.nikolaev.7@gmail.com>
2021-03-22 19:04:57 +0300

    message added

---
+++ company_dict.py
@@ -0,0 +1,12 @@
+company_map = {
+    "INTC": "Intel",
+    "APPL": "Apple",
+    "GOOG": "Google",
+    "AMD": "AMD",
+    "TSM": "Taiwan Semiconductor Manufacturing",
+    "AMAZN": "Amazon",
+    "NVDA": "Nvidia",
+    "TSLA": "Tesla",
+    "MSFT": "Microsoft",
+    "FB": "Facebook",
+}
--- main.py
+++ main.py
@@ -3,8 +3,7 @@ import stonks_pb2
 import stonks_pb2_grpc
 
 from news_sent import company_sentiment
-
-example_companies = ["INTC", "APPL", "GOOG", "AMD", "TSM", "AMAZN", "NVDA", "TSLA", "MSFT", "FB",]
+from company_dict import company_map
 
 
 def get_priority(client, company):
@@ -14,7 +13,9 @@ def get_priority(client, company):
 
 def get_top_five(client, companies):
     top_companies = sorted(companies, key=lambda c: get_priority(client, c), reverse=True)[:5]
-    return sorted(top_companies, key=lambda c: get_rating(c), reverse=True)
+    top_companies = [company_map.get(c, "S&P 500") for c in top_companies]
+    top_companies = [(c, get_rating(c)) for c in top_companies]
+    return sorted(top_companies, key=lambda c: c[1], reverse=True)
 
 
 def get_rating(company):
@@ -22,18 +23,18 @@ def get_rating(company):
 
 
 def send_ratings(client, companies):
-    name_rating = {}
-    for c in companies:
-        name_rating[c] = get_rating(c)
-    message = str(name_rating)
+    message = ''
+    for i, (name, score) in enumerate(companies):
+        message += f"{i+1}. {name}, sentiment score: {score}.\n"
+    message += 'Good luck!'
     client.TelegramNotification(stonks_pb2.TelegramRequest(Message=message))
 
 
 def main():
     channel = grpc.insecure_channel('localhost:50051')
     stub = stonks_pb2_grpc.StonksApiStub(channel)
-    
-    top_companies = get_top_five(stub)
+    sample_companies = ["INTC", "APPL", "GOOG", "AMD", "TSM", "AMAZN", "NVDA", "TSLA", "MSFT", "FB", ]
+    top_companies = get_top_five(stub, sample_companies)
     send_ratings(stub, top_companies)
 
 
